214cbf37c90ff04f86c403a504f965e6a61179db
Dan Wanek <dan.wanek@gmail.com>
2009-09-03 09:48:26 -0500

    Added more IE event methods.

--- SeleniumIE.rb
+++ SeleniumIE.rb
@@ -40,6 +40,7 @@ class SeleniumIERecorder
 	def eventHandler(ev_name, *ev_args)
 		if methodExists?(ev_name)
 			method(ev_name).call(*ev_args)
+			puts "-------------------------------------------------" if $DEBUG
 		end
 	end
 
@@ -81,14 +82,62 @@ class SeleniumIERecorder
 	public
 
 	# http://msdn.microsoft.com/en-us/library/aa768280(VS.85).aspx
-	def BeforeNavigate2(*ev_args)
-		puts "BeforeNavigate2: #{ev_args.class.to_s}"
+	def BeforeNavigate2(pDisp, url, flags, targetFrameName, postData, headers, cancel)
+		puts "Navigating to #{url}"
+		puts "\t Target Frame: #{targetFrameName}"
+	end
+
+	# http://msdn.microsoft.com/en-us/library/aa768329(VS.85).aspx
+	def DocumentComplete(pDisp, url)
+	end
+
+	# http://msdn.microsoft.com/en-us/library/aa768334(VS.85).aspx
+	def NavigateComplete2(pDisp, url)
+		puts "** Navigation to #{url} complete **"
+	end
+
+	# http://msdn.microsoft.com/en-us/library/bb268221(VS.85).aspx
+	def NavigateError(pDisp, url, targetFrameName, statusCode, cancel)
+		puts "Navigation ERROR occured (#{url}):  Status Code #{statusCode}"
+	end
+
+	# http://msdn.microsoft.com/en-us/library/aa768336(VS.85).aspx
+	def NewWindow2(ppDisp, cancel)
+		puts "NewWindow2 event fired"
+	end
+
+	# http://msdn.microsoft.com/en-us/library/aa768337(VS.85).aspx
+	def NewWindow3(ppDisp, cancel, dwFlags, bstrUrlContext, bstrUrl)
+		puts "NewWindow3 event fired"
 	end
 
 	# http://msdn.microsoft.com/en-us/library/cc136549(VS.85).aspx
 	def OnQuit(*ev_args)
 		@browser.visible = false
 		throw :done
+	end
+
+	# http://msdn.microsoft.com/en-us/library/aa768347(VS.85).aspx
+	def ProgressChange(nProgress, nProgressMax)
+	end
+
+	# http://msdn.microsoft.com/en-us/library/aa768348(VS.85).aspx
+	def PropertyChange(sProperty)
+		puts "Property Changed: #{sProperty}"
+	end
+
+	# http://msdn.microsoft.com/en-us/library/aa768349(VS.85).aspx
+	def StatusTextChange(sText)
+		puts "Status Text Changed: #{sText}"
+	end
+
+	# http://msdn.microsoft.com/en-us/library/aa768350(VS.85).aspx
+	def TitleChange(sText)
+		puts "Title changed to #{sText}"
+	end
+
+	# http://msdn.microsoft.com/en-us/library/aa768358(VS.85).aspx
+	def WindowStateChanged(dwFlags, dwValidFlagsMask)
 	end
 
 end
